3fd4c5be6339b023cee22151bc535e1702c20e23
Peter Wu <petercoin@users.noreply.github.com>
2019-09-09 15:09:58 +0800

    Create main.py
    Initial version.

---
+++ main.py
@@ -0,0 +1,45 @@
+import sys, string, os
+from enum import Enum
+from collections import defaultdict
+
+ddf = defaultdict(list)
+
+def enumerateFiles(path):
+    files = []
+    pathToEnum = path
+    items = os.listdir(pathToEnum)
+
+    for item in items:
+        if os.path.isdir(path + "\\" + item):
+            files += enumerateFiles(path + "\\" + item)
+        elif os.path.isfile(path + "\\" + item):
+            ddf[path].append(item)
+            files.append(path + "\\" + item)
+
+    return files
+
+if __name__ == "__main__":
+    argv = sys.argv
+    pathToEnumerate = argv[1]
+    files = enumerateFiles(pathToEnumerate)
+
+    print(".OPTION EXPLICIT    ; Generate errors")
+    print(".Set CabinetFileCountThreshold=0;")
+    print(".Set FolderFileCountThreshold=0;")
+    print(".Set FolderSizeThreshold=0;")
+    print(".Set MaxCabinetSize=0;")
+    print(".Set MaxDiskFileCount=0;")
+    print(".Set MaxDiskSize=0;")
+    print(".Set CompressionType=MSZIP;")
+    print(".Set Cabinet=on;")
+    print(".Set Compress=on;")
+    print("; Specify file name for new cab file;")
+    print(".Set CabinetNameTemplate=kb.cab;")
+
+    for key in ddf:
+        print("")
+        cmd = ".Set DestinationDir=" + key
+        print(cmd)
+        
+        for value in ddf[key]:
+            print("\"" + key + "\\" + value + "\"")
